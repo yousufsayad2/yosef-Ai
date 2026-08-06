7a5500fc1a0dfb1b59034ee2d388ffa4df82e88e
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-07 00:04:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,45 @@
 import streamlit as st
-from google import genai
+from openai import OpenAI
 
-st.set_page_config(
-    page_title="Yosef AI",
-    page_icon="🤖",
-    layout="centered"
-)
+st.set_page_config(page_title="Yosef AI", page_icon="🤖")
 
 st.title("🤖 Yosef AI")
 
-api_key = st.text_input(
-    "Gemini API Key",
-    type="password"
-)
+api_key = st.text_input("OpenRouter API Key", type="password")
 
 if api_key:
 
-    client = genai.Client(api_key=api_key)
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    for msg in st.session_state.messages:
-        with st.chat_message(msg["role"]):
-            st.markdown(msg["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
     prompt = st.chat_input("اكتب رسالتك...")
 
     if prompt:
 
         st.session_state.messages.append(
-            {
-                "role": "user",
-                "content": prompt
-            }
+            {"role": "user", "content": prompt}
         )
 
         with st.chat_message("user"):
             st.markdown(prompt)
 
         try:
-            response = client.models.generate_content(
-                model="gemini-2.0-flash",
-                contents=prompt
+
+            response = client.chat.completions.create(
+                model="meta-llama/llama-3.1-8b-instruct:free",
+                messages=st.session_state.messages
             )
 
-            answer = response.text
+            answer = response.choices[0].message.content
 
             st.session_state.messages.append(
                 {
@@ -58,7 +52,7 @@ if api_key:
                 st.markdown(answer)
 
         except Exception as e:
-            st.error(f"حدث خطأ: {e}")
+            st.error(str(e))
 
 else:
-    st.info("🔑 أدخل مفتاح Gemini API للبدء.")
+    st.info("أدخل مفتاح OpenRouter API")

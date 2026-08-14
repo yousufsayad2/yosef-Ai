6bbe5609acaae04e161b87b99cc1f1946a34d496
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-14 16:42:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,8 +53,8 @@ if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
 
-            try:
-        content = [{"type": "text", "text": prompt}]
+                try:
+                content = [{"type": "text", "text": prompt}]
 
         image_file = None
 

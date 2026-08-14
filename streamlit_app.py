55062d6ad3d22a0e38d895e4b66e0e3772845951
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-14 18:31:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,8 +48,9 @@ if prompt:
       
             
             response = client.chat.completions.create(
-        model="openrouter/free",
-        messages=api_messages
+    model="openrouter/free",
+    messages=api_messages
+            )
     )
 
     answer = response.choices[0].message.content

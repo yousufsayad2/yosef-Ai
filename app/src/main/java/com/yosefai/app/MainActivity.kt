eecfb92d98becfc5a6723801fb207fe9b04f87ba
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-15 19:22:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yosefai/app/MainActivity.kt
+++ app/src/main/java/com/yosefai/app/MainActivity.kt
@@ -1,29 +1,54 @@
 package com.yosefai.app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        webView = WebView(this)
 
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.loadsImagesAutomatically = true
-        webView.settings.allowFileAccess = true
-        webView.settings.allowContentAccess = true
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            loadsImagesAutomatically = true
+            allowFileAccess = true
+            allowContentAccess = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            userAgentString = userAgentString + " YosefAIApp"
+        }
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
         webView.loadUrl(
             "https://yosef-ai-2pzqmuovs4ggxc5wyvz8ce.streamlit.app/"
         )
 
         setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
     }
 }

2365142a8ffb2e7537a158c066229ab8cc129d5d
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-15 18:06:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yosefai/app/MainActivity.kt
+++ app/src/main/java/com/yosefai/app/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.yosefai.app
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Yosef AI"
+        textView.textSize = 28f
+
+        setContentView(textView)
     }
 }

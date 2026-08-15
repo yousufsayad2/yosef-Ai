5629ea90735b3511b8724acb5da99d6fb855cabc
streamlit_app.py <yosaifabonasr@gmail.com>
2026-08-15 22:41:25 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yosefai/app/MainActivity.kt
+++ app/src/main/java/com/yosefai/app/MainActivity.kt
@@ -2,18 +2,39 @@ package com.yosefai.app
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
+
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val textView = TextView(this)
-        textView.text = "Yosef AI"
-        textView.textSize = 32f
-        textView.setPadding(40, 40, 40, 40)
+        webView = WebView(this)
 
-        setContentView(textView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.databaseEnabled = true
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(
+            "https://yosef-ai-2pzqmuovs4ggxc5wyvz8ce.streamlit.app/"
+        )
+
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
